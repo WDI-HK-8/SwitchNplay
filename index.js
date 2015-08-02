195257fe95931e029031a0f3a18a772351c59795
raphaeldieu <dieuraphael@gmail.com>
2015-08-02 16:03:21 +0800

    Initial commit / server started

---
+++ index.js
@@ -0,0 +1,42 @@
+var Hapi = require('hapi');
+var Path =require('path');
+
+var server = new Hapi.Server();
+
+//server config
+server.connection({
+  host: '0.0.0.0',
+  port: 3000,
+  routes: {
+    cors: {
+      headers: ["Access-Control-Allow-Credentials"],
+      credentials: true
+    }
+  }
+});
+
+var plugins = [
+  {//Mongodb
+    register: require('hapi-mongodb'),
+    options:{
+      url: 'mongodb://127.0.01:27017/switchnplay',
+      settings: {
+        db: {
+          native_parser: false
+        }
+      }
+    }
+  }
+];
+
+//start the server
+server.register(plugins, function(err){
+  if (err){
+    throw err;
+  }
+  server.start(function(){
+    console.log('server running at : '+server.info.uri);
+  });
+
+
+});
